271f1c913937da3d31d8040fb32e4170c7cc32d2
sofia-kildyaeva <81563074+sofia-kildyaeva@users.noreply.github.com>
2022-06-17 00:45:10 -0700

    Add files via upload

---
+++ Starter/ConsoleApplication/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine(line);
+        }
+    }
+}
